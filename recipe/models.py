a4902222e18ff63dd7744368c2d894e48b218546
Mohd Razzeq <rzq272@gmail.com>
2023-07-30 15:46:39 +0800

    feat: Add Recipe app

---
+++ recipe/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.urls import reverse
+
+def userpath(instance, filename):
+    return 'user{0}/{1}'.format(instance.user.id, filename)
+
+# Create your models here.
+class Recipe(models.Model):
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=255)
+    ingredients = models.CharField(max_length=255)
+    images = models.ImageField(upload_to=userpath, height_field=None, width_field=None, blank=True, null=True)
+
+
+    class Meta:
+        verbose_name = "Recipe"
+        verbose_name_plural = "Recipes"
+
+    def __str__(self):
+        return self.name
+
